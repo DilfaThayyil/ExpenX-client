737eacf9a3c408daf039f725174ca7ab12c478be
DilfaThayyil <dilfathayyil@gmail.com>
2025-01-01 23:48:26 +0530

    api setup with axios

---
+++ src/services/axios/axios.ts
@@ -0,0 +1,47 @@
+import axios from "axios";
+// import Swal from "sweetalert2";
+import { BACKENDENDPOINT } from "../../utility/env";
+const axiosInstance = axios.create({
+  baseURL: BACKENDENDPOINT,
+  withCredentials: true,
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response) {
+      if (error.response.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true;
+
+        try {
+          await axiosInstance.post("/user/auth/refresh-token");
+          return axiosInstance(originalRequest);
+        } catch (err) {
+          window.location.href = "/";
+          localStorage.clear();
+          return Promise.reject(err);
+        }
+      } else if (error.response.status === 403) {
+        localStorage.clear();
+        console.log('you have been blocked!')
+        // Swal.fire({
+        //   icon: "error",
+        //   title: "Access Denied",
+        //   text: "You have been blocked. Please contact support.",
+        //   timer: 6000,
+        //   timerProgressBar: true,
+        //   willClose: () => {
+        //     window.location.href = "/";
+        //   },
+        // });
+        return Promise.reject(error);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
