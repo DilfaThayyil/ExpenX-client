c14075b79cbe7424489b1662aa95db073fa388c9
DilfaThayyil <dilfathayyil@gmail.com>
2025-01-04 23:31:45 +0530

    api for user auth updated

--- src/services/user/AuthServices.ts
+++ src/services/user/AuthServices.ts
@@ -1,6 +1,6 @@
 import { AxiosError } from "axios";
 import axiosInstance from "../axios/axios";
-const BASEURL = "http://localhost:3000/api/auth"
+const BASEURL = "http://localhost:3000/api/user/auth"
 
 interface IcreateUser {
   username: string
@@ -60,18 +60,18 @@ export const verifyOtp = async (email: string, otp: string) => {
   }
 };
 
-// export const resendOtp = async (email: string) => {
-//   try {
-//     console.log(email);
+export const resendOtp = async (email: string) => {
+  try {
+    console.log(email);
 
-//     const response = await axiosInstance.post(`${BASEURL}/resendOtp`, {
-//       email,
-//     });
-//     return response.data;
-//   } catch (err) {
-//     return err;
-//   }
-// };
+    const response = await axiosInstance.post(`${BASEURL}/resendOtp`, {
+      email,
+    });
+    return response.data;
+  } catch (err) {
+    return err;
+  }
+};
 
 export const userLogin = async (email: string, password: string) => {
   try {
@@ -92,7 +92,7 @@ export const userLogin = async (email: string, password: string) => {
 
 export const handleforgetpassword = async (email: string) => {
   try {
-    const response = await axiosInstance.post(`${BASEURL}/forgotPassword`, {
+    const response = await axiosInstance.post(`${BASEURL}/forgetPassword`, {
       email,
     });
     if (response) {
@@ -106,42 +106,42 @@ export const handleforgetpassword = async (email: string) => {
   }
 };
 
-// export const handleforgetpasswordOtp = async (email: string, otp: string) => {
-//   try {
-//     const response = await axiosInstance.post(
-//       "/user/auth/verifyforgetpassword",
-//       { email, otp }
-//     );
-//     return response.data;
-//   } catch (err) {
-//     const axiosError = err as AxiosError;
-//     if (axiosError.response) {
-//       const errorData = axiosError.response.data as ErrorResponse;
-//       return errorData.error;
-//     }
-//   }
-// };
+export const handleforgetpasswordOtp = async (email: string, otp: string) => {
+  try {
+    const response = await axiosInstance.post(
+      `${BASEURL}/forgetPassOtp`,
+      { email, otp }
+    );
+    return response.data;
+  } catch (err) {
+    const axiosError = err as AxiosError;
+    if (axiosError.response) {
+      const errorData = axiosError.response.data as ErrorResponse;
+      return errorData.error;
+    }
+  }
+};
 
-// export const changePassword = async (email: string, password: string) => {
-//   try {
-//     const response = await axiosInstance.post(`${BASEURL}/user/auth/changePassword`, {
-//       email,
-//       password,
-//     });
-//     return response.data.message;
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
+export const changePassword = async (email: string, password: string) => {
+  try {
+    const response = await axiosInstance.post(`${BASEURL}/changePassword`, {
+      email,
+      password,
+    });
+    return response.data.message;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-// export const googleAuth = async (userCredential:string) => {
-//   try {
-//     const response = await axiosInstance.post(`/user/auth/googleauth/${userCredential}`);
-//     return response.data
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
+export const googleAuth = async (userCredential:string) => {
+  try {
+    const response = await axiosInstance.post(`${BASEURL}/googleAuth/${userCredential}`);
+    return response.data
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 // export const logout = () => {
 //   localStorage.removeItem("userAccessToken");
