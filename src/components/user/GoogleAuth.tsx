ed5cb61bab6147bbfb867b80d37afd0432d4aa5b
DilfaThayyil <dilfathayyil@gmail.com>
2025-01-10 18:26:51 +0530

    feat: GoogleAuth updated as component

--- src/components/user/GoogleAuth.tsx
+++ src/components/user/GoogleAuth.tsx
@@ -4,19 +4,27 @@ import useShowToast from '../../customHook/showToaster'
 import { useNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 
-const GoogleAuth = () => {
+interface GoogleAuthProps{
+  role: 'user'| 'advisor'
+}
+
+const GoogleAuth:React.FC<GoogleAuthProps> = ({role}) => {
     const navigate=useNavigate()
     const Toast=useShowToast()
     const handleGoogle = async (userCredential:CredentialResponse) => {
         if(userCredential.credential){
-            
             const user = jwtDecode(userCredential.credential)
             console.log("user : ",user)
             const userData= await googleAuth(user)
             console.log("userData : ",userData)
          if(userData?.message){
+          if(role==='advisor'){
+            Toast(userData.message,'success',true)
+            setTimeout(()=>navigate("/advisor"),1000)
+          }else{
             Toast(userData.message,'success',true)
             setTimeout(() => navigate("/"), 1000);
+          }
          }
         }
       };
