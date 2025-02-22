f3f9924accb97474bc11854a8a8bd8db74a3667f
DilfaThayyil <dilfathayyil@gmail.com>
2025-02-22 23:41:49 +0530

    fix: resolving some essential bugs

--- src/pages/user/Expenses.tsx
+++ src/pages/user/Expenses.tsx
@@ -7,7 +7,7 @@ import {
     SelectItem,
     SelectTrigger,
     SelectValue,
-} from "@/components/ui/select";
+} from "@/components/ui/select"
 import { Input } from "@/components/ui/input";
 import {
     Table,
